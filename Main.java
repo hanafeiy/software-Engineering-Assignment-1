2c2328317b04e132f67d4cb9a7e94389a7a96127
hanafeiy <80263639+hanafeiy@users.noreply.github.com>
2026-03-01 22:49:42 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Simple Calculator ===");
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Choose operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Cannot divide by zero!");
+                    sc.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                sc.close();
+                return;
+        }
+
+        System.out.println("Result: " + num1 + " " + op + " " + num2 + " = " + result);
+
+        sc.close();
+    }
+}
